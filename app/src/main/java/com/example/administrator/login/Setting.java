09ea9d3056fc8bf8543e1c14bbc3d8b4f93e79e3
lt066 <413089882@qq.com>
2015-12-30 14:21:18 +0800

    Signed-off-by: lt066 <413089882@qq.com>

---
+++ app/src/main/java/com/example/administrator/login/Setting.java
@@ -0,0 +1,43 @@
+package com.example.administrator.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.anotoolbar);
+        toolbar.setTitle("");
+        TextView title = (TextView) findViewById(R.id.bar_text);
+        title.setText("登录");
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // TODO Auto-generated method stub
+        if(item.getItemId() == android.R.id.home)
+        {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
